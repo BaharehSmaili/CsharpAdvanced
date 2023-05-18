1fa78955ba52cd82149184216111c5f1e827eb9e
bahareh.Smi <bahareh_smaili_1990@yahoo.com>
2023-05-18 14:43:50 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CsharpAdvanced.LINQquerySyntax;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Created by : Bahareh.Smi");
+        Console.WriteLine("Date : 1402/02/28");
+        Console.WriteLine();
+
+        Console.WriteLine("Restriction Operators (where) : ");
+        new RestrictionOperators().LowNumbers();
+
+    }
+}
