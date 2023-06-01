03f45a230c848a9971b349bde02e8700c91b0823
bahareh smaili <bahaareh_smaili_1990@yahoo.com>
2023-06-01 12:51:41 +0330

    add sample fore Aggregator,Conversion,Element,GenerateSequences,Grouping,Join,Ordering,QuantifyingMembers,Sequence,Set

--- LINQquerySyntax/GenerateSequences.cs
+++ LINQquerySyntax/GenerateSequences.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace CsharpAdvanced.LINQquerySyntax
 {
-    internal class GenerateSequences
+    public class GenerateSequences
     {
+        public int RangeOfIntegers()
+        {
+            #region generate-range
+            var numbers = from n in Enumerable.Range(100, 50)
+                          select (Number: n, OddEven: n % 2 == 1 ? "odd" : "even");
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine("The number {0} is {1}.", n.Number, n.OddEven);
+            }
+            #endregion
+            return 0;
+        }
+
+        public int RepeatNumber()
+        {
+            #region generate-repeat
+            var numbers = Enumerable.Repeat(7, 10);
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            #endregion
+            return 0;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,13 +9,18 @@ internal class Program
         Console.WriteLine("Created by : Bahareh.Smi");
         Console.WriteLine("Date : 1402/02/28");
         Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
 
         Console.WriteLine("Restriction Operators (where) : ");
         new RestrictionOperators().LowNumbers();
+        Console.WriteLine();
 
-
+        Console.WriteLine("-----------------------------------------------------------------------------");
         Console.WriteLine("Created by : Bahareh.Smi");
         Console.WriteLine("Date : 1402/03/04");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------------------------------------");
         Console.WriteLine();
 
         Console.WriteLine("Restriction Operators (where-property) : ");
@@ -152,5 +157,318 @@ internal class Program
         new PartitionOperators().IndexedSkipWhile();
         Console.WriteLine();
 
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine("Created by : Bahareh.Smi");
+        Console.WriteLine("Date : 1402/03/11");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderby-syntax) : ");
+        new OrderingOperators().OrderbySyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderby-property) : ");
+        new OrderingOperators().OrderbyProperty();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderby-user-types) : ");
+        new OrderingOperators().OrderByProducts();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderby-custom-comparer) : ");
+        new OrderingOperators().OrderByWithCustomComparer();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderbydescending-syntax) : ");
+        new OrderingOperators().OrderByDescendingSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (orderby-descending-type) : ");
+        new OrderingOperators().OrderProductsDescending();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (desc-custom-comparer) : ");
+        new OrderingOperators().DescendingCustomComparer();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (thenby-syntax) : ");
+        new OrderingOperators().ThenBySyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (thenby-custom) : ");
+        new OrderingOperators().ThenByCustom();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (thenby-ordering) : ");
+        new OrderingOperators().ThenByDifferentOrdering();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (thenby-custom-descending) : ");
+        new OrderingOperators().CustomThenByDescending();
+        Console.WriteLine();
+
+        Console.WriteLine("Ordering Operators (reverse) : ");
+        new OrderingOperators().OrderingReversal();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (groupby-syntax) : ");
+        new GroupingOperators().GroupingSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (groupby-property) : ");
+        new GroupingOperators().GroupByProperty();
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (groupby-category) : ");
+        new GroupingOperators().GroupByCategory();
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (nested-groupby) : ");
+        new GroupingOperators().NestedGrouBy();
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (roupby-custom-comparer) : ");
+        new GroupingOperators().GroupByCustomComparer();
+        Console.WriteLine();
+
+        Console.WriteLine("Grouping Operators (nested-groupby-custom) : ");
+        new GroupingOperators().NestedGroupByCustom();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (distinct-syntax) : ");
+        new SetOperators().DistinctSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (distinct-property-values) : ");
+        new SetOperators().DistinctPropertyValues();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (union-syntax) : ");
+        new SetOperators().UnionSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (union-query-results) : ");
+        new SetOperators().UnionOfQueryResults();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (intersect-syntax) : ");
+        new SetOperators().IntersectSynxtax();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (intersect-different-queries) : ");
+        new SetOperators().IntersectQueryResults();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (distinct-syntax) : ");
+        new SetOperators().DifferenceOfSets();
+        Console.WriteLine();
+
+        Console.WriteLine("Set Operators (except-syntax) : ");
+        new SetOperators().DifferenceOfQueries();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Conversion Operators (convert-to-array) : ");
+        new ConversionOperators().ConvertToArray();
+        Console.WriteLine();
+
+        Console.WriteLine("Conversion Operators (convert-to-list) : ");
+        new ConversionOperators().ConvertToList();
+        Console.WriteLine();
+
+        Console.WriteLine("Conversion Operators (convert-to-dictionary) : ");
+        new ConversionOperators().ConvertToDictionary();
+        Console.WriteLine();
+
+        Console.WriteLine("Conversion Operators (convert-to-type) : ");
+        new ConversionOperators().ConvertSelectedItems();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Element Operators (first-element) : ");
+        new ElementOperators().FirstElement();
+        Console.WriteLine();
+
+        Console.WriteLine("Element Operators (first-matching-element) : ");
+        new ElementOperators().FirstMatchingElement();
+        Console.WriteLine();
+
+        Console.WriteLine("Element Operators (first-or-default) : ");
+        new ElementOperators().MaybeFirstElement();
+        Console.WriteLine();
+
+        Console.WriteLine("Element Operators (first-matching-or-default) : ");
+        new ElementOperators().MaybeFirstMatchingElement();
+        Console.WriteLine();
+
+        Console.WriteLine("Element Operators (element-at) : ");
+        new ElementOperators().ElementAtPosition();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Generate Sequences (generate-range) : ");
+        new GenerateSequences().RangeOfIntegers();
+        Console.WriteLine();
+
+        Console.WriteLine("Generate Sequences (generate-repeat) : ");
+        new GenerateSequences().RepeatNumber();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Quantifying Members (any-matches) : ");
+        new QuantifyingMembers().AnyMatchingElements();
+        Console.WriteLine();
+
+        Console.WriteLine("Quantifying Members (any-grouped) : ");
+        new QuantifyingMembers().GroupedAnyMatchedElements();
+        Console.WriteLine();
+
+        Console.WriteLine("Quantifying Members (all-match) : ");
+        new QuantifyingMembers().AllMatchedElements();
+        Console.WriteLine();
+
+        Console.WriteLine("Quantifying Members (all-grouped) : ");
+        new QuantifyingMembers().GroupedAllMatchedElements();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (count-syntax) : ");
+        new AggregatorOperators().CountSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (count-conditional) : ");
+        new AggregatorOperators().CountConditional();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (nested-count) : ");
+        new AggregatorOperators().NestedCount();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (grouped-count) : ");
+        new AggregatorOperators().GroupedCount();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (sum-syntax) : ");
+        new AggregatorOperators().SumSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (sum-of-projection) : ");
+        new AggregatorOperators().SumProjection();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (grouped-sum) : ");
+        new AggregatorOperators().SumGrouped();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (min-syntax) : ");
+        new AggregatorOperators().MinSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (min-projection) : ");
+        new AggregatorOperators().MinProjection();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (min-grouped) : ");
+        new AggregatorOperators().MinGrouped();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (min-each-group) : ");
+        new AggregatorOperators().MinEachGroup();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (max-syntax) : ");
+        new AggregatorOperators().MaxSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (max-projection) : ");
+        new AggregatorOperators().MaxProjection();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (max-grouped) : ");
+        new AggregatorOperators().MaxGrouped();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (max-each-group) : ");
+        new AggregatorOperators().MaxEachGroup();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (average-syntax) : ");
+        new AggregatorOperators().AverageSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (average-projection) : ");
+        new AggregatorOperators().AverageProjection();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (average-grouped) : ");
+        new AggregatorOperators().AverageGrouped();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (aggregate-syntax) : ");
+        new AggregatorOperators().AggregateSyntax();
+        Console.WriteLine();
+
+        Console.WriteLine("Aggregator Operators (aggregate-seeded) : ");
+        new AggregatorOperators().SeededAggregate();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Sequence Operations (equal-sequence) : ");
+        new SequenceOperations().EqualSequence();
+        Console.WriteLine();
+
+        Console.WriteLine("Sequence Operations (not-equal-sequence) : ");
+        new SequenceOperations().Linq97();
+        Console.WriteLine();
+
+        Console.WriteLine("Sequence Operations (concat-series) : ");
+        new SequenceOperations().ConcatSeries();
+        Console.WriteLine();
+
+        Console.WriteLine("Sequence Operations (concat-projections) : ");
+        new SequenceOperations().ConcatProjection();
+        Console.WriteLine();
+
+        Console.WriteLine("Sequence Operations (dot-product) : ");
+        new SequenceOperations().DotProduct();
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine("Join Operations (cross-join) : ");
+        new JoinOperations().CrossJoinQuery();
+        Console.WriteLine();
+
+        Console.WriteLine("Join Operations (group-join) : ");
+        new JoinOperations().GroupJoinQquery();
+        Console.WriteLine();
+
+        Console.WriteLine("Join Operations (cross-group-join) : ");
+        new JoinOperations().CrossGroupJoin();
+        Console.WriteLine();
+
+        Console.WriteLine("Join Operations (left-outer-join) : ");
+        new JoinOperations().LeftOuterJoin();
+        Console.WriteLine();
     }
 }
